1959e8cee74df8cb874843495f6d083a5b71bd41
Jacob Mathis <jamathis77@gmail.com>
2018-01-16 11:44:05 -0500

    added crud for recipes

---
+++ test/test-for-recipes.js
@@ -0,0 +1,88 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+const {app, runServer, closeServer} = require('../server');
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('Recipes', function(){
+  before(function(){
+    return runServer();
+  });
+
+  after(function(){
+    return closeServer();
+  });
+
+  it('should list recipes on get', function(){
+    return chai.request(app);
+    get('/recipes')
+    .then(function(res){
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('array');
+      expect(res.body.length).to.be.at.least(1);
+
+      const expectedKeys = ['id', 'name', 'ingredients'];
+      res.body.forEach(function(item){
+        expect(item).to.be.a('object');
+        expect(item).to.include.keys(expectedKeys);
+      })
+    })
+  });
+
+  it('should add an item on post', function(){
+    const newItem = {name: 'coffee', checked: false};
+    return chai.request(app)
+      post('/recipes')
+      .send(newItem)
+      .then(function(res) {
+        expect(res).to.have.status(201);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('object');
+        expect(res.body).to.include.keys('id', 'name', 'checked');
+        expect(res.body.id).to.not.equal(null);
+        // response should be deep equal to `newItem` from above if we assign
+        // `id` to it from `res.body.id`
+        expect(res.body).to.deep.equal(Object.assign(newItem, {id: res.body.id}));
+      });
+  });
+
+  const updateData = {
+    name: 'foo',
+    checked: true
+  };
+
+  return chai.request(app)
+    get('/recipes')
+    .then(function(res) {
+      updateData.id = res.body[0].id;
+      return chai.request(app)
+        .put(`/recipes/${updateData.id}`)
+        .send(updateData);
+    })
+    .then(function(res) {
+      expect(res).to.have.status(200);
+      expect(res).to.be.json;
+      expect(res.body).to.be.a('object');
+      expect(res.body).to.deep.equal(updateData);
+    });
+});
+
+
+it('should delete items on DELETE', function() {
+  return chai.request(app)
+    get('/recipes')
+    .then(function(res) {
+      return chai.request(app)
+        .delete(`/recipes/${res.body[0].id}`);
+    })
+    .then(function(res) {
+      expect(res).to.have.status(204);
+    });
+});
+
+
+})
